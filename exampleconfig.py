6a215b23feb389bb30dc0a0968f02260a241f767
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2023-07-16 16:16:17 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -9,16 +9,17 @@ class Development(Config):
     ALIVE_NAME = "Your value"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "Your value"
-    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+    # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "Your value"
-    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "Your value"
-    TG_BOT_USERNAME = "Your value"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
     COMMAND_HAND_LER = "."
-    # sudo enter the id of sudo users userid's in that array
-    SUDO_USERS = []
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
+    # External plugins repo
+    EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
+    # if you need badcat plugins set "True"
+    BADCAT = "False"
